f3252d6762e2996bca8bf6f5617435eadbeeb7cb
Marifer <mfvl.1186@gmail.com>
2023-03-20 15:35:58 -0300

    actualizar main.js con función de navbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,21 @@
-// Import our custom CSS
-import '../scss/styles.scss'
-// Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
-import { myFunction } from './lib/index.js';
+import { myFunction } from '../lib/index';
 
 myFunction();
+
+// When the user scrolls down 80px from the top of the document,
+// resize the navbar's padding and the logo's font size
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById('navbar').style.padding = '30px 10px';
+    document.getElementById('logo').style.fontSize = '25px';
+  } else {
+    document.getElementById('navbar').style.padding = '80px 10px';
+    document.getElementById('logo').style.fontSize = '35px';
+  }
+}
+
+// eslint-disable-next-line func-names
+window.onscroll = function () {
+  scrollFunction();
+};
