9f47d01ad695ff02193d39cb4008f266852b1aff
xochitl <unmananaarchivos@gmail.com>
2023-03-21 23:06:04 -0600

    importar router y paths e inicianilizar

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,15 @@
+import { router } from "../router/router.js"
+import { paths } from "../router/routes.js"
+
+function initializeRouter() {
+  const ROUTER = new router(paths);
+  ROUTER.load('home')
+}
+
+window.onload(initializeRouter())
+
 // When the user scrolls down 80px from the top of the document,
 // resize the navbar's padding and the logo's font size
-
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById('navbar').style.padding = '30px 10px';
@@ -15,5 +24,13 @@ function scrollFunction() {
 window.onscroll = function () {
   scrollFunction();
 };
+
+
+document.getElementById("about").addEventListener("click", ROUTER.load('about'))
+
+
+/* const contentRoot = document.getElementById("root");
+contentRoot.innerHTML =  */
+
 
 
