c71ba380da49b7ae162f53465090f1fb352754ec
Marifer Villalobos <mfvl.1186@gmail.com>
2023-04-09 19:54:35 -0400

    eliminar barril y dejar archivo como firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+
+
+// Web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAMoIZnaqiWN7MrGggAkrPwJqTMUN-_xXE",
+  authDomain: "outsy-mxg.firebaseapp.com",
+  databaseURL: "https://outsy-mxg-default-rtdb.firebaseio.com",
+  projectId: "outsy-mxg",
+  storageBucket: "outsy-mxg.appspot.com",
+  messagingSenderId: "1058170420655",
+  appId: "1:1058170420655:web:06d17c4cc4440b35d0035a",
+  measurementId: "G-MF6SX17B18"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+
+
+
+export default app;
+
+
+
+
+
