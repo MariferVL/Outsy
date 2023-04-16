da2be768971a63257a990efd6c728177b88933bc
Gaabs <ggaabrielagomez@gmail.com>
2023-04-15 22:08:06 -0600

    Vista feed arreglo navbar, publicaciones css

--- src/views/feed.js
+++ src/views/feed.js
@@ -1,80 +1,56 @@
 export const feed = `  
-<body class="bg-light">
-    
-<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark" aria-label="Main navigation">
-  <div class="container-fluid">
-    <a class="navbar-brand">Outsy</a>
-    <button class="navbar-toggler p-0 border-0" type="button" id="navbarSideCollapse" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page">Dashboard</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link">Notificaciones</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link">Perfil</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link">Cambia de cuenta</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Menú</a>
-          <ul class="dropdown-menu">
-            <li><a id= "post" class="dropdown-item" >Crear invitación</a></li>
-            <li><a class="dropdown-item">Ajustes</a></li>
-            <li><a id="signOut" class="dropdown-item">Cerrar Sesión</a></li>
-
-          </ul>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
+<body class="bg-light">  
+<!-- Navbar -->
+<nav class="navbar navbar-expand-md navbar-dark">
+<div class="container-fluid " id="navbar-feed">
+  <a class="navbar-brand" href="">
+    <img src="images/logo1.png" alt="Logo" width="75" height="75" class="d-inline-block align-text-top">
+  </a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+    aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse " id="navbarSupportedContent">
+    <ul class="navbar-nav ms-auto mb-2 mb-lg-0 ">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="">Dashboard</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="">Notificaciones</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="">Perfil</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="form" role="button" data-bs-toggle="dropdown"
+          aria-expanded="false">
+          Menú
+        </a>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" id="post">Crear Publicación</a></li>
+          <li>
+            <hr class="dropdown-divider">
+          </li>
+          <li><a class="dropdown-item" href="">Ajustes</a></li>
+          <li><a class="dropdown-item" href="">Cerrar Sesión</a></li>
+        </ul>
+      </li>
+    </ul>
+    <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Busqueda">
+      <button class="btn btn-outline-light goldButton" type="submit">Buscar</button>
+    </form>
   </div>
+</div>
 </nav>
 
-<div class="nav-scroller bg-body shadow-sm">
-  <nav class="nav" aria-label="Secondary navigation">
-    <a class="nav-link active" aria-current="page">Dashboard</a>
-    <a class="nav-link">
-      Friends
-      <span class="badge text-bg-light rounded-pill align-text-bottom">27</span>
-    </a>
-    <a class="nav-link">Explore</a>
-    <a class="nav-link">Suggestions</a>
-    <a class="nav-link">Link</a>
-    <a class="nav-link">Link</a>
-    <a class="nav-link">Link</a>
-    <a class="nav-link">Link</a>
-    <a class="nav-link">Link</a>
-  </nav>
-</div>
 
-<main id="feed" class="container">
-<section id="posts">
-<h2>Invitaciones Recientes</h2>
-<div id="postContainer"></div>
-</section>
-  <div class="d-flex align-items-center p-3 my-3 text-white bg-purple rounded shadow-sm">
-    <img class="me-3" src="" alt="" width="48" height="38">
-    <div class="lh-1">
-      <h1 class="h6 mb-0 text-white lh-1">Outsy</h1>
-      <small>Desde 2023</small>
-    </div>
-  </div>
-
-
+  <main id="feed" class="container">
+  <section id="posts">
   <div class="my-3 p-3 bg-body rounded shadow-sm">
-    <h6 class="border-bottom pb-2 mb-0">Suggestions</h6>
-    <div class="d-flex text-muted pt-3">
-      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
+    <h2 class="border-bottom pb-2 mb-0">Publicaciones Recientes</h2>
+    <div class="d-flex text-muted pt-3" id="postContainer">
       <div class="pb-3 mb-0 small lh-sm border-bottom w-100">
         <div class="d-flex justify-content-between">
           <strong class="text-gray-dark">Full Name</strong>
@@ -84,28 +60,6 @@ export const feed = `
       </div>
     </div>
     <div class="d-flex text-muted pt-3">
-      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
-      <div class="pb-3 mb-0 small lh-sm border-bottom w-100">
-        <div class="d-flex justify-content-between">
-          <strong class="text-gray-dark">Full Name</strong>
-          <a>Follow</a>
-        </div>
-        <span class="d-block">@username</span>
-      </div>
-    </div>
-    <div class="d-flex text-muted pt-3">
-      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
-      <div class="pb-3 mb-0 small lh-sm border-bottom w-100">
-        <div class="d-flex justify-content-between">
-          <strong class="text-gray-dark">Full Name</strong>
-          <a>Follow</a>
-        </div>
-        <span class="d-block">@username</span>
-      </div>
-    </div>
-    <small class="d-block text-end mt-3">
-      <a>All suggestions</a>
-    </small>
   </div>
 </main>
 
