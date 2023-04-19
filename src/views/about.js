7632ea56040ecfe81cec1395a8379d0e9f3b32c1
Marifer Villalobos <mfvl.1186@gmail.com>
2023-04-19 09:42:34 -0400

    corregir id de ourpictures css

--- src/views/about.js
+++ src/views/about.js
@@ -59,7 +59,7 @@ export const about = `
   <!-- About Us -->
 <section class="aboutus">
   <div class="container py-5">
-    <div i="ourPictures" class="row d-flex justify-content-center">
+    <div id="ourPictures" class="row d-flex justify-content-center">
       <div id="about-text" class="col-md-10 col-xl-8 text-center">
         <h1 class="fw-bold mb-4">Acerca de Nosotras</h1>
         <p class="mb-4 pb-2 mb-md-5 pb-md-0">
